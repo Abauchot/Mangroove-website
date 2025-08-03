08b7f81c091d7970e3dc54a585af418be56afaae
G8ite <amandine.pda@gmail.com>
2025-08-03 20:37:26 +0200

    fix(backend): Tentative de réparation

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f public/index.php ]; then
+  echo "Symfony non initialisé, création du projet..."
+  composer create-project symfony/skeleton . --no-interaction
+  composer require api
+  composer require nelmio/api-doc-bundle
+  composer require symfony/orm-pack
+  composer require symfony/maker-bundle --dev
+  composer require doctrine/doctrine-bundle
+fi
+
+php -S 0.0.0.0:8000 -t public
